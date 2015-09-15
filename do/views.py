6858c506d55a720c7b47c437d2ed09406b3ebef7
Dan Knowlton <dtknowlton@gmail.com>
2015-09-15 05:59:18 -0700

    Fix typo in database view.

--- do/views.py
+++ do/views.py
@@ -16,6 +16,6 @@ def db(request):
     """ Print out the state of the database as it currently stands. """
     users = User.objects.all()
     todos = DoItem.objects.all()
-    catogories = Category.objects.all()
+    categories = Category.objects.all()
 
-    return render(request, 'db.html', {'users': users, 'todos': todos, 'categories': categories})
+    return render(request, 'db.html', {'users': users, 'todos': todos, 'categories': categories}
