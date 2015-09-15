bf69610cc30656474df01f38ac716721a8cd8ff5
Dan Knowlton <dtknowlton@gmail.com>
2015-09-15 07:45:41 -0700

    Use index template for index view.

--- do/views.py
+++ do/views.py
@@ -8,8 +8,7 @@ import requests
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES', 3))
-    return HttpResponse('Hello! ' * times)
+    return render(request, 'index.html')
 
 
 def db(request):
