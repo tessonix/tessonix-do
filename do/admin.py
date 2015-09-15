4c04da5dab5eefb0147abf400c16c81642116025
Dan Knowlton <dtknowlton@gmail.com>
2015-09-15 06:07:09 -0700

    Add todo models to admin page.

--- do/admin.py
+++ do/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Category, DoItem, User
+admin.site.register(Category)
+admin.site.register(DoItem)
+admin.site.register(User)
